e95a568f5853faa5e3985dea9d72d0446a95d92b
kelvinempire <kelvinewurum@gmail.com>
2024-11-11 16:50:22 +0100

    mongo2

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import db from "./db/conn.js";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/movies", async (req, res) => {
+  const collection = db.collection("movies");
+  const movies = await collection.find({}).limit(10).toArray();
+  console.log(movies);
+  res.json(movies);
+});
+
+app.get("/users", async (req, res) => {
+  const collection = db.collection("users");
+  const users = await collection.find().limit(10).toArray();
+  console.log(users);
+  res.json(users);
+});
+
+app.listen(port, () =>
+  console.log(`server is running on port http://localhost:${port}`)
+);
