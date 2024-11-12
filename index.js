03178aefc63ba6bd6f3bbca2eb60cd45ef183979
kelvinempire <kelvinewurum@gmail.com>
2024-11-12 13:57:03 +0100

    newChanges

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./db/conn.js";
+
 const app = express();
 const port = 3000;
 
@@ -10,14 +11,12 @@ app.get("/", (req, res) => {
 app.get("/movies", async (req, res) => {
   const collection = db.collection("movies");
   const movies = await collection.find({}).limit(10).toArray();
-  console.log(movies);
   res.json(movies);
 });
 
 app.get("/users", async (req, res) => {
   const collection = db.collection("users");
   const users = await collection.find().limit(10).toArray();
-  console.log(users);
   res.json(users);
 });
 
